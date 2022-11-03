246868887760dfca66c817da2af8b23e939db231
Dew <kittiponjunlapak@gmail.com>
2022-11-03 13:18:39 +0700

    add Profile

---
+++ screens/Profile.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {StyleSheet} from 'react-native';
+
+import {Box, Center, Heading, NativeBaseProvider} from 'native-base';
+
+const Home = () => {
+  const [show, setShow] = React.useState(false);
+
+  const handleClick = () => setShow(!show);
+  return (
+    <NativeBaseProvider>
+      <Center w="100%">
+        <Box safeArea p="2" w="90%" maxW="290" py="8">
+          <Heading
+            size="lg"
+            color="#105A88"
+            fontWeight="900"
+            fontSize={50}>
+            Profile
+          </Heading>
+        </Box>
+      </Center>
+    </NativeBaseProvider>
+  );
+};
+
+const styles = StyleSheet.create({});
+export default Profile;
